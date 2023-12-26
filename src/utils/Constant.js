7c08631f018dd25a6627493101b116fa29cc81d7
Nguyễn Tấn Phát <nguyentanphat.12041998@gmail.com>
2023-12-26 13:11:23 +0700

    Fix api

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,6 +1,6 @@
 export const URL = {
     GRAPH_TOKEN: "http://auth2.voip24h.vn/api/token",
     GRAPH: "http://graph.voip24h.vn/",
-    REGISTER_PUSH: "http://203.162.56.235:1998/register_push_for_sdk",
-    UNREGISTER_PUSH: "http://203.162.56.235:1998/unregister_push_for_sdk"
+    REGISTER_PUSH: "http://14.225.251.99:1998/register_push_for_sdk",
+    UNREGISTER_PUSH: "http://14.225.251.99:1998/unregister_push_for_sdk"
 }
